101ac112584f38953b402aab50be3e87a7489756
masud90895 <masudhossainmbs129@gmail.com>
2023-10-01 23:02:51 +0600

    user complete

---
+++ src/app/modules/users/users.controller.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { UsersService } from './users.service';
+
+// get all users
+const getAllUsers = catchAsync(async (req: Request, res: Response) => {
+  const token = req.headers?.authorization;
+
+  const result = await UsersService.getAllUsers(token as string);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User retrieved  successfully!',
+    data: result,
+  });
+});
+
+// get single user
+
+const getSingleUser = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params?.id;
+  const result = await UsersService.getSingleUser(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User fetched successfully!',
+    data: result,
+  });
+});
+
+// update user
+
+const updateUser = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params?.id;
+  const data = req.body;
+  const result = await UsersService.updateUser(id, data);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User Updated successfully!',
+    data: result,
+  });
+});
+
+// delete user
+
+const deleteUser = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params?.id;
+
+  const result = await UsersService.deleteUser(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User Deleted successfully!',
+    data: result,
+  });
+});
+
+export const UsersController = {
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+};
---
+++ src/app/modules/users/users.service.ts
@@ -0,0 +1,149 @@
+import { User } from '@prisma/client';
+import httpStatus from 'http-status';
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
+import ApiError from '../../../errors/ApiError';
+import { jwtHelpers } from '../../../helpers/jwtHelpers';
+import prisma from '../../../shared/prisma';
+
+// get all users
+const getAllUsers = async (token: string) => {
+  let decodedToken;
+
+  try {
+    decodedToken = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
+  } catch (error) {
+    throw new ApiError(httpStatus.FORBIDDEN, 'Invalid access Token');
+  }
+
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      id: decodedToken?.userId,
+    },
+  });
+
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'User Not Exist');
+  }
+
+  const result = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      contactNo: true,
+      address: true,
+      profileImg: true,
+      orders: true,
+      reviewAndRatings: true,
+    },
+  });
+
+  return result;
+};
+
+// get single user
+
+const getSingleUser = async (id: string) => {
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'User not found !!');
+  }
+
+  const result = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      contactNo: true,
+      address: true,
+      profileImg: true,
+      orders: true,
+      reviewAndRatings: true,
+    },
+  });
+
+  return result;
+};
+
+// update user
+
+const updateUser = async (id: string, data: Partial<User>) => {
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'User not found !!');
+  }
+
+  const result = await prisma.user.update({
+    where: {
+      id,
+    },
+    data,
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      contactNo: true,
+      address: true,
+      profileImg: true,
+    },
+  });
+
+  return result;
+};
+
+// delete user
+
+const deleteUser = async (id: string) => {
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'User not found !!');
+  }
+
+  const result = await prisma.user.delete({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      contactNo: true,
+      address: true,
+      profileImg: true,
+      orders: true,
+      reviewAndRatings: true,
+    },
+  });
+
+  return result;
+};
+
+export const UsersService = {
+  getAllUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+};
