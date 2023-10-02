2b9223363e0255e120d8dc960f255e5af4bab1fb
masud90895 <masudhossainmbs129@gmail.com>
2023-10-02 11:37:57 +0600

    profile complete

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { UserAuthRoutes } from '../modules/userAuth/userAuth.routes';
 import { UsersRoutes } from '../modules/users/users.routes';
 import { CategoriesRoutes } from '../modules/category/category.routes';
+import { ProfileRoutes } from '../modules/profile/profile.routes';
 
 const router = express.Router();
 
@@ -17,6 +18,10 @@ const moduleRoutes = [
   {
     path: '/categories',
     route: CategoriesRoutes,
+  },
+  {
+    path: '/profile',
+    route: ProfileRoutes,
   },
 ];
 
